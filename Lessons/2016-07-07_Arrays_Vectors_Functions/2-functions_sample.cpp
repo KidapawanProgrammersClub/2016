fc38ae2a322cad1315793209a75cea7d7f06e823
Jboy Flaga <jboyflaga2@gmail.com>
2016-07-07 20:32:15 +0800

    Create 2-functions_sample.cpp

---
+++ Lessons/2016-07-07_Arrays_Vectors_Functions/2-functions_sample.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int powerFunctionForInts(int base, int exponent)
+{
+	int product = base;
+	for (int i = 1; i < exponent; i++)
+	{
+		product *= base;
+	}
+
+	return product;
+}
+
+int main()
+{
+	// using the power function for integers that we created above
+	int powIntResult = powerFunctionForInts(2, 3);
+	cout << powIntResult << endl;
+
+	// using the pow() function the form standard library math.h
+	double resultOfPowFromStdMathLib = pow(2, 3);
+	cout << resultOfPowFromStdMathLib << endl;
+
+	cin.get();
+	return 0;
+}
